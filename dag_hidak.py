ba2689276d153cb7c93b6e1871c4059f8117ee5c
cheol2Y <gkgkrhrjt@gmail.com>
2024-03-05 14:10:27 +0900

    'ccc'

--- dag_hidak.py
+++ dag_hidak.py
@@ -12,7 +12,7 @@ from pykospacing import Spacing
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2024, 3, 5),
+    'start_date': datetime(2024, 3, 4),
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 1,
--- dag_naver.py
+++ dag_naver.py
@@ -11,7 +11,7 @@ import re
 import json
 import requests
 import os
-from pykospacing import Spacing
+
 
 
 sys.path.append('/home/ubuntu/airflow_work/dags/script') # Add the path to your scripts here
@@ -23,12 +23,12 @@ from scrape_utils import save_to_json, read_from_json, append_to_csv, is_today, 
 from scrape_doctor_profiles import scrape_doctor_profiles
 from scrape_info import scrape_info
 from scrape_details import scrape_details
-from processing import preprocess_dataframe
+from processing import *
 
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2024, 3, 5),
+    'start_date': datetime(2024, 3, 4),
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 1,
@@ -70,60 +70,32 @@ def scrape_details_for_all_docs(**kwargs):
 
 
 
-def remove_greeting2(text):
-    text = re.sub(r'하이닥.*?입니다\.', '', text)
-    text = re.sub(r'네이버.*?입니다\.', '', text)
-    text = re.sub(r'닥톡.*?입니다\.', '', text)
-    text = re.sub(r'대한의사협회.*?입니다\.', '', text)
-    # text = re.sub(r'\p{Hangul}+과전문의.*?입니다\.', '', text)
-    return text
-def remove_greeting1(text):
-    return re.sub(r'안녕하세요\.', '', text)
-def remove_special_chars(text):
-    return text.replace('\\xa0', '')
+
 
 spacing = Spacing()
-def preprocess_json(data_list):
-    preprocessed_data = []
-    for data in data_list:
-        if not data['Date']:  # Date가 비어있는 경우
-            preprocessed_data = data
-            return preprocessed_data
-        else:
-            # 전처리 적용
-            # data['title'] = spacing(data['title'])
-            # data['question'] = spacing(data['question'])
-            data['answer'] = ''.join(data['answer'])  # 리스트를 문자열로 변환
-            data['question'] = remove_greeting1(data['question'])  # 인사말 제거
-            data['answer'] = remove_greeting1(data['answer'])  # 인사말 제거
-            data['answer'] = remove_greeting2(data['answer'])  # 인사말 제거
-            data['answer'] = remove_special_chars(data['answer'])  # 특수 문자 제거
-            # data['answer'] = spacing(data['answer'])
-            data['question'] = remove_special_chars(data['question'])  # 특수 문자 제거
-            if '삭제' not in data['question']:  # '삭제' 키워드가 없는 경우만 추가
-                preprocessed_data.append(data)
-    return preprocessed_data
-
-
-
-
-
 def preprocess_csv_file(**kwargs):
-    date_format = "%Y.%m.%d."
-    today = datetime.now().strftime(date_format)
-    csv_file_path = f'./csv_folder/today_naver_QnA/details_{today}.csv'  # CSV 파일의 경로
+    today = datetime.today()
+    today_str = today.strftime('%Y-%m-%d')
+    csv_file_path = f'./csv_folder/today_naver_QnA/details_{today_str}_QNA.csv'  # CSV 파일의 경로
 
     # CSV 파일 읽기
     df = pd.read_csv(csv_file_path)
+    
+    df_cleaned = df.dropna()
 
-    # 전처리 함수 적용
-    preprocessed_df = preprocess_dataframe(df)
+    processed_data = [preprocess_data(row) for index, row in df_cleaned.iterrows()]
+    
+    # 결과를 DataFrame으로 변환
+    preprocessed_df = pd.DataFrame(processed_data)
 
     # 전처리된 데이터를 새로운 CSV 파일로 저장
-    preprocessed_csv_file_path = f'./csv_folder/today_naver_QnA/preprocessed_{today}.csv'
+    preprocessed_csv_file_path = f'./csv_folder/today_naver_QnA/details_{today_str}_QNA_pros.csv'
     preprocessed_df.to_csv(preprocessed_csv_file_path, index=False)
 
     return preprocessed_csv_file_path
+
+
+
 
 def insert_data_to_postgres(**kwargs):
     ti = kwargs['ti']
@@ -187,11 +159,11 @@ preprocess_csv_task = PythonOperator(
     dag=dag,
 )
 
-insert_to_postgres_task = PythonOperator(
-    task_id='insert_to_postgres',
-    python_callable=insert_data_to_postgres,
-    dag=dag,
-)
+# insert_to_postgres_task = PythonOperator(
+#     task_id='insert_to_postgres',
+#     python_callable=insert_data_to_postgres,
+#     dag=dag,
+# )
 
 
 
